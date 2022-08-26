7bce5380891aab48b940eaaa9b3d09262ccfb433
Dylan <dylanad2@illinois.edu>
2022-08-25 22:12:52 -0500

    Starter

---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+const urll = process.env.REACT_APP_URL;
+const accountss = process.env.REACT_APP_ACCOUNTS;
+
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: urll,
+      accounts: [ accountss ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
