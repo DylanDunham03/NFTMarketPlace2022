1c84723e2192d3ac33806a211686d4ed84f4e850
Dylan <dylanad2@illinois.edu>
2022-09-18 17:21:35 -0500

    Updated

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -7,29 +7,29 @@ import { useState } from "react";
 export default function Marketplace() {
 const sampleData = [
     {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
+        "name": "Creature",
+        "description": "Creature WRLD #1432",
+        "website":"https://www.creature.world/",
+        "image":"https://gateway.pinata.cloud/ipfs/QmYAYZCPtWa8pHkty6gu6fuADpm4tYYGcFdawEbUQDzrTm",
+        "price":"0.3ETH",
         "currentlySelling":"True",
         "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
     },
     {
-        "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
+        "name": "BoredApe",
+        "description": "BAPE #6578",
+        "website":"https://boredapeyachtclub.com/",
+        "image":"https://gateway.pinata.cloud/ipfs/QmdhToAw17iaGWbv5VG1dvsAzbFe3vDwF4A7M7iANZYGfU",
         "price":"0.03ETH",
         "currentlySelling":"True",
         "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
     },
     {
-        "name": "NFT#3",
-        "description": "Alchemy's Third NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
+        "name": "APE",
+        "description": "1 of 1 Ape",
+        "website":"https://boredapeyachtclub.com/",
+        "image":"https://gateway.pinata.cloud/ipfs/Qmf68ehfaGVntjEZ1Yn5zHR9NiFS57mbTPiSNuvBXXTTTH",
+        "price":"0.15ETH",
         "currentlySelling":"True",
         "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
     },
@@ -87,6 +87,7 @@ return (
                     return <NFTTile data={value} key={index}></NFTTile>;
                 })}
             </div>
+
         </div>            
     </div>
 );
--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -104,12 +104,12 @@ async function buyNFT(tokenId) {
                         {console.log(data.seller, "seller")}
                         {console.log(data.owner, "owner")}
 
-                    { currAddress == data.owner || currAddress == data.seller ?
-                        <div className="text-emerald-700">You are the owner of this NFT</div>
+                    { currAddress === data.owner || currAddress === data.seller ?
+                        <div className="text-blue-300">You are the owner of this NFT</div>
                         : <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
                     }
                     
-                    <div className="text-green text-center mt-3">{message}</div>
+                    <div className="text-blue text-center mt-3">{message}</div>
                     </div>
                 </div>
             </div>
